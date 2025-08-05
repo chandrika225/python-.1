4411e7ee31d7731a0d446fe2456c146a07e410ab
chandrika225 <sankarachandrika@gmail.com>
2025-08-05 20:55:11 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,90 @@
+#str1 = print(input().title())
+
+
+ 
+#nums = "1 2 3 4"
+#num_list = nums.split(",")
+#print(num_list)
+
+
+
+'''list_a=['python is ',' progr','mming l','ngu','ge']
+string_a="a".join(list_a)
+print(string_a)
+
+
+
+n = input().lower().split()
+n = "_".join(n)
+print(n)
+
+
+
+n = input().lower().title()
+n = "".join(n)
+print(n)
+
+
+
+n = input().upper()
+n = "".join(n)
+print(n)
+
+
+
+
+n = input().swapcase()
+n = "".join(n)
+print(n)
+
+
+
+n = "All silver tea cups"
+vowels = "aeiouAEIOU"
+for i in n:
+    if i in vowels:
+        print(i,end=",")
+
+
+
+n = "muni"
+vowels = "aeiouAEIOU"
+for i in n:
+    if i in vowels:
+        print(i)
+
+
+
+
+
+n = "muniswari"
+vowels = "aeiou"
+for i in n:
+    if i not in vowels:
+        print(i)
+
+
+n = input().swapcase()
+n = "".join(n)
+print(n)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
