4ae90b7adb403713b2afcf145a1ca8902fc7e7f1
chandrika225 <sankarachandrika@gmail.com>
2025-08-05 20:59:01 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,16 @@
+'''l1 = input().split()
+l1 = sorted(l1)
+print(l1)
+
+
+
+l1 = input().split()
+l2 = list(map(int,l1))
+print(l2)
+
+
+
+
+l1 = list(map(int,input().split()))
+l1.sort()
+print(l1)'''
