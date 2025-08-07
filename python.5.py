3d38c80561b0d6c51a974eac134af5bff2dba911
chandrika225 <sankarachandrika@gmail.com>
2025-08-07 09:16:06 +0530

    Add files via upload

---
+++ python.5.py
@@ -0,0 +1,84 @@
+'''list_1 = [1,2,3,4,5,6,7,8,9,10]
+target = 8
+start = 0
+end = 9
+while(start<=end):
+    middle = (start+end)//2  
+    if list_1[middle]==target:
+        index =(middle)
+    elif list_1[middle]>target:
+        end = middle-1
+    elif list_1[middle]<target:
+        start=middle+1
+        
+
+
+//right angle tringle//
+
+
+n=int(input())
+for i in range(1,n+1):
+    print("@ "*i)
+
+
+
+
+n=int(input())
+for i in range(5,n-1,-1):
+        print("* "*i)'''
+
+
+
+
+
+
+'''n=int(input())
+for i in range(n,0,-1):
+        print("* "*i)'''
+
+
+
+'''n=int(input())
+for i in range(1,n+1):
+    str1=""
+    for j in range(1,i+1):
+        str1=str1+str(j)+" "
+    print(str1)'''
+    
+    
+
+
+
+n=int(input())
+for i in range(1,n+1):
+    str1=" "
+    for j in range(1,i+1):
+        str1=str1+str(j)+" "
+    print(str1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
